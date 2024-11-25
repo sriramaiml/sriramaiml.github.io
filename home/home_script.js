705acf489eafffab114f81c43b58736ffebf6c9c
sriramaiml <59199524+sriramaiml@users.noreply.github.com>
2024-11-25 19:56:50 +0530

    deleted

--- home/home_script.js
+++
@@ -1,9 +0,0 @@
-// // Example of a jQuery task (can be customized or expanded as needed)
-
-// $(document).ready(function() {
-//     // Example task: Log a message when a product card button is clicked
-//     $('.product-card button').click(function() {
-//         window.location.href = "aboutus/aboutus.html";
-//     });
-// });
-
--- models/models_script.js
+++
@@ -1,6 +0,0 @@
-// jQuery for button actions
-$(document).ready(function() {
-    $('.model-btn').click(function() {
-        alert("More information about this model will be available soon!");
-    });
-});
